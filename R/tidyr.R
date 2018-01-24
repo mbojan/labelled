5de6f41c013e0df7f8be7da5a010e5c37879cb08
Michal Bojanowski <michal2992@gmail.com>
2018-01-24 17:55:05 +0100

    Added subsetting labelled_spss

--- R/tidyr.R
+++ R/tidyr.R
@@ -1,9 +1,27 @@
 #' tidyr-related functions
 #' @name tidyr
 
+#' @rdname tidyr
+#' @details
+#' `as.list.labelled`
 #' @export
 as.list.labelled <- function(x, ...) {
   res <- vector("list", length(x))
   for (i in seq_along(x)) res[[i]] <- x[i]
+  res
+}
+
+#' @rdname tidyr
+#' @details
+#' `[` method copies labels, na_values, and na_range
+#' @export
+"[.labelled_spss" <- function(x, ...) {
+  res <- labelled_spss(
+    NextMethod(),
+    labels = attr(x, "labels"),
+    na_values = na_values(x),
+    na_range = na_range(x)
+  )
+  var_label(res) <- var_label(x)
   res
 }
